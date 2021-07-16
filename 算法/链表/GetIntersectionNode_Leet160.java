7f54d79c576d487c5222d9dc029295ff9fbdd9a1
caixi <caixi169@gmail.com>
2021-07-16 23:02:10 +0800

    develop : leetcode 160, 链表相交，双指针解决
    Signed-off-by: caixi <caixi169@gmail.com>

---
+++ 算法/链表/GetIntersectionNode_Leet160.java
@@ -0,0 +1,44 @@
+package com.android.test1.node;
+
+/**
+ * @describe :
+ * @usage :
+ * <p>
+ * </p>
+ * Created by caixi on 7/16/21.
+ */
+public class GetIntersectionNode_Leet160 {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 双指针法
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            a = (a != null) ? a.next : headB;
+            b = (b != null) ? b.next : headA;
+        }
+        return a;
+    }
+}
