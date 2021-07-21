a30951d2d64d3777cbcc82848cc88fc00a83719e
caixi <caixi169@gmail.com>
2021-07-21 16:39:21 +0800

    develop : leetcode 202的解法，使用hash set来解决快乐数的问题
    Signed-off-by: caixi <caixi169@gmail.com>

---
+++ 算法/哈希表/IsHappy_Leet202.java
@@ -0,0 +1,57 @@
+package com.android.test1.hash;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @describe :
+ * @usage :
+ * <p>
+ * 编写一个算法来判断一个数 n 是不是快乐数。
+ * <p>
+ * 「快乐数」定义为：
+ * <p>
+ * 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+ * 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+ * 如果 可以变为  1，那么这个数就是快乐数。
+ * 如果 n 是快乐数就返回 true ；不是，则返回 false 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：19
+ * 输出：true
+ * 解释：
+ * 12 + 92 = 82
+ * 82 + 22 = 68
+ * 62 + 82 = 100
+ * 12 + 02 + 02 = 1
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/happy-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * </p>
+ * Created by caixi on 7/21/21.
+ */
+public class IsHappy_Leet202 {
+
+    public boolean isHappy(int n) {
+        Set<Integer> result = new HashSet<>();
+        while(n != 1 && !result.contains(n)) {
+            result.add(n);
+            n = getNextNum(n);
+        }
+        return n == 1;
+    }
+
+    private int getNextNum(int n) {
+        int res = 0;
+        while(n > 0) {
+            int tmp = res % 10;
+            res += tmp * tmp;
+            n = n / 10;
+        }
+        return res;
+    }
+}
