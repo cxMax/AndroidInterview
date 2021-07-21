17bff834acc6b44f3fff336b4a16e332b7d41227
caixi <caixi169@gmail.com>
2021-07-21 15:27:14 +0800

    develop : leetcode 242题， 使用hash表（数组来实现）来解决，两个单词是否使用统一的字母组成
    Signed-off-by: caixi <caixi169@gmail.com>

---
+++ 算法/哈希表/IsAnagram_Leet242.java
@@ -0,0 +1,56 @@
+package com.android.test1.hash;
+
+/**
+ * @describe :
+ * @usage :
+ * <p>
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ * <p>
+ * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-anagram
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * </p>
+ * Created by caixi on 7/21/21.
+ */
+public class IsAnagram_Leet242 {
+
+    /**
+     * 通过数组实现hash算法
+     * 他这个主要是小写字母， 小写字母26个
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram(String s, String t) {
+        if (s == null || s.length() == 0
+                || t == null || t.length() == 0
+                || s.length() != t.length()) {
+            return false;
+        }
+        int[] record = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            record[s.charAt(i) - 'a'] += 1;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            record[t.charAt(i) - 'a'] -= 1;
+        }
+        for (int i : record) {
+            if (i != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
