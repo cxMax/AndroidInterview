b867dc7a2fd4ad6911ecd3d0c9bd0835e65cfdd4
caixi <caixi169@gmail.com>
2021-08-27 10:13:39 +0800

    develop : leetcode 146题，手写LruCache算法
    Signed-off-by: caixi <caixi169@gmail.com>

--- algorithm/Leetcode/app/src/main/java/com/cxmax/leetcode/hash/LRUCache_Leet146.java
+++ algorithm/Leetcode/app/src/main/java/com/cxmax/leetcode/hash/LRUCache_Leet146.java
@@ -29,29 +29,35 @@ public class LRUCache_Leet146 {
      * 他有头、尾、容量 参数
      */
     public static class DoubleList {
-        Node head = new Node(0, 0); //这个是构造的 我理解为虚拟节点
-        Node tail = new Node(0, 0); //这个是构造的 我理解为虚拟节点
+        Node head = new Node(0, 0); //这个理解为第一个节点的前置节点。
+        Node tail = new Node(0, 0); //这个理解为最后一个节点的后置节点
         int size;
+
+        public DoubleList() {
+            size = 0;
+            head.next = tail;
+            tail.prev = head;
+        }
 
         /**
          * 添加到队头元素
          * @param node
          */
         public void addFirst(Node node) {
-            Node headNext = head.next;
+            Node headNext = head.next; // 原来的第一个节点
+            head.next = node;
+            headNext.prev = node;
+
             node.prev = head;
             node.next = headNext;
-            headNext.prev = node;
-            head.next = node;
             size++;
 
         }
 
         /**
          * 移除最后一个元素
-         * @param node
          */
-        public Node removeTail(Node node) {
+        public Node removeTail() {
             Node lastNode = tail.prev;
             remove(lastNode);
             return lastNode;
@@ -91,8 +97,33 @@ public class LRUCache_Leet146 {
         // 想要考虑在不在缓存中
         if (map.containsKey(key)) {
             // 在缓存中，就要做两个事， 就是更新队列，移动到队头
-
+            cache.remove(map.get(key));
+            cache.addFirst(node);
+            map.put(key, node); //这里因为node变了，所以要重新设置一次
+        } else {
+            // 如果不在缓存中, 要判断队列是否溢出，溢出的话要移除最后一个
+            if (cache.size() == capacity) {
+                Node lastNode = cache.removeTail();
+                map.remove(lastNode.key);
+            }
+            cache.addFirst(node);
+            map.put(key, node);
         }
+    }
 
+    /**
+     * 获取值
+     * @param key
+     * @return
+     */
+    public int get(int key) {
+        //先要判断缓存中，有没有这个值
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        int val = map.get(key).value;
+        // 更新位置
+        put(key, val);
+        return val;
     }
 }
