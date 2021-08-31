9ea1c620210958138a20f59e8cabce0298d8731e
caixi <caixi169@gmail.com>
2021-08-31 14:35:31 +0800

    develop : leetcode 144 二叉树前序遍历， 遍历的方式来实现
    Signed-off-by: caixi <caixi169@gmail.com>

--- algorithm/Leetcode/app/src/main/java/com/cxmax/leetcode/tree/PreorderTraversal_Leet144.java
+++ algorithm/Leetcode/app/src/main/java/com/cxmax/leetcode/tree/PreorderTraversal_Leet144.java
@@ -2,6 +2,7 @@ package com.cxmax.leetcode.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @describe :
@@ -14,6 +15,12 @@ import java.util.List;
  */
 public class PreorderTraversal_Leet144 {
 
+
+    /**
+     * 这个是递归的形式来做
+     * @param root
+     * @return
+     */
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         preOrder(root, result);
@@ -27,7 +34,36 @@ public class PreorderTraversal_Leet144 {
         result.add(root.val);
         preOrder(root.left, result);
         preOrder(root.right, result);
-
     }
 
+    /**
+     * 采用遍历的方式来做
+     * 根 左 右 , 入栈顺序 根 右 左， 先进后出
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal1(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        //  这个用堆栈来做
+        Stack<TreeNode> stack = new Stack<>();
+        // 先把根节点 push进去
+        stack.push(root);
+        // 开始遍历
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            //然后是右、右节点
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+
+        }
+        return result;
+    }
 }
