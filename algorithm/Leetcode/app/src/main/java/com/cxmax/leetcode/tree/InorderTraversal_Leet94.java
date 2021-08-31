972b3b0c68f43bd222151037fda0fab00d9f07dc
caixi <caixi169@gmail.com>
2021-08-31 15:01:15 +0800

    develop : leetcode 94题，二叉树中序遍历，使用遍历的方式来实现
    Signed-off-by: caixi <caixi169@gmail.com>

--- algorithm/Leetcode/app/src/main/java/com/cxmax/leetcode/tree/InorderTraversal_Leet94.java
+++ algorithm/Leetcode/app/src/main/java/com/cxmax/leetcode/tree/InorderTraversal_Leet94.java
@@ -2,6 +2,7 @@ package com.cxmax.leetcode.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @describe :
@@ -14,6 +15,11 @@ import java.util.List;
  */
 public class InorderTraversal_Leet94 {
 
+    /**
+     * 递归的实现， 就是左、中、右
+     * @param root
+     * @return
+     */
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         inOrder(root, result);
@@ -27,5 +33,34 @@ public class InorderTraversal_Leet94 {
         inOrder(node.left, result);
         result.add(node.val);
         inOrder(node.right, result);
+    }
+
+    /**
+     * 中序遍历的实现。
+     * 核心就是， 先一路左节点走到底入栈，然后依次移除，在遍历右节点。
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal1(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while(cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                // 一直入栈最左边的节点
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                // 这个时候开始获取值
+                cur = stack.pop();
+                result.add(cur.val);
+                // 这个时候在赋值右节点
+                cur = cur.right;
+            }
+        }
+        return result;
     }
 }
