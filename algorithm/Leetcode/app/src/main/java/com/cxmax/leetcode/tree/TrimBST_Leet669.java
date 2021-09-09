cbb18aefcd25d2a18b8907128cedb4422de68d13
caixi <caixi169@gmail.com>
2021-09-09 10:22:43 +0800

    develop : leetcode 669，修剪二叉树，使用递归来做的
    Signed-off-by: caixi <caixi169@gmail.com>

---
+++ algorithm/Leetcode/app/src/main/java/com/cxmax/leetcode/tree/TrimBST_Leet669.java
@@ -0,0 +1,39 @@
+package com.cxmax.leetcode.tree;
+
+/**
+ * @describe :
+ * @usage :
+ * <p>
+ *     669. 修剪二叉搜索树
+ *     给你二叉搜索树的根节点 root ，同时给定最小边界low 和最大边界 high。
+ * </p>
+ * Created by caixi on 9/9/21.
+ */
+public class TrimBST_Leet669 {
+
+    /**
+     * 感觉也是 根、 左 、 右的方式递归呢
+     * @param root
+     * @param low
+     * @param high
+     * @return
+     */
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+        // 根节点，小于最小区间， 那就看比他大的右节点
+        if (root.val < low) {
+            trimBST(root.right, low, high);
+        }
+        // 那就看比他小的 左节点
+        if (root.val > high) {
+            trimBST(root.left, low, high);
+        }
+        // 再确定左、右两个节点
+        trimBST(root.left, low, high);
+        trimBST(root.right, low, high);
+        return root;
+    }
+
+}
