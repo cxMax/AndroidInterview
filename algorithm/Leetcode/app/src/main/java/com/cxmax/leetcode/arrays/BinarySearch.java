cbec440c5826e2b4a017221470caa98be51dcf23
caixi <caixi169@gmail.com>
2021-09-20 18:29:32 +0800

    develop : 二分查找
    Signed-off-by: caixi <caixi169@gmail.com>

--- algorithm/Leetcode/app/src/main/java/com/cxmax/leetcode/arrays/BinarySearch.java
+++ algorithm/Leetcode/app/src/main/java/com/cxmax/leetcode/arrays/BinarySearch.java
@@ -122,4 +122,26 @@ public class BinarySearch {
         return -1;
     }
 
+    /**
+     * 二分查找
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int binarySearch5(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right) {
+            int mid = left + ((right - left) >> 1;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid -1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
 }
