09df42bc625c92c615794ce64e7353aec5d8ad1e
caixi <caixi169@gmail.com>
2022-07-27 16:26:48 +0800

    feat: 三刷 leetcode, 反转字符串
    https://leetcode.cn/problems/reverse-string-ii/
    
    Signed-off-by: caixi <caixi169@gmail.com>

--- algorithm/Leetcode/app/src/main/java/com/cxmax/third/string/ReverseStr.java
+++ algorithm/Leetcode/app/src/main/java/com/cxmax/third/string/ReverseStr.java
@@ -1,30 +1,35 @@
-package com.cxmax.selftest.string;
+package com.cxmax.third.string;
 
 /**
  * 541. 反转字符串 II
  * <p>
  * https://leetcode-cn.com/problems/reverse-string-ii/
  * <p>
- * <p>
- * Created by caixi on 2022/2/22.
+ * Created by caixi on 2022/7/27.
  */
 public class ReverseStr {
 
     /**
-     * 前2k个字符，翻转前k个字符
+     * 每2k个字符，翻转前k个，
+     *
+     * 1. 两点，每2k个字符，就是+= 2k；
+     * 2. 剩余部分，大于k 小于2k的，取剩余长度
      *
      * @param s
      * @param k
      * @return
      */
     public String reverseStr(String s, int k) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
         char[] chars = s.toCharArray();
-        for (int i = 0; i < chars.length; i += (2 * k)) {
-            // 这里理解，第一个2k， 要翻转前k个
+        for (int i = 0; i < chars.length; i += 2 * k) {
+            // 确定起始点
             int left = i;
-            // > k 或者 < 2k，要取剩余的值
+            // 这里是处理大于k，不足2k的处理
             int right = Math.min(chars.length - 1, i + k - 1);
-            while(right > left) {
+            while (left < right) {
                 char tmp = chars[left];
                 chars[left] = chars[right];
                 chars[right] = tmp;
@@ -33,6 +38,6 @@ public class ReverseStr {
             }
         }
         return new String(chars);
-    }
 
+    }
 }
