17ac78101d161ccd45f863cae349634686c614a3
caixi <caixi169@gmail.com>
2022-07-31 13:19:18 +0800

    feat: 三刷 leetcode 遍历实现 145. 二叉树的后序遍历
    Signed-off-by: caixi <caixi169@gmail.com>

--- algorithm/Leetcode/app/src/main/java/com/cxmax/third/tree/PostorderTraversal.java
+++ algorithm/Leetcode/app/src/main/java/com/cxmax/third/tree/PostorderTraversal.java
@@ -1,7 +1,9 @@
 package com.cxmax.third.tree;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
@@ -32,5 +34,36 @@ public class PostorderTraversal {
         traversal(node.left, result);
         traversal(node.right, result);
         result.add(node.val);
+    }
+
+    /**
+     * 后序，通过遍历的方式来做
+     *
+     * 左、右、根
+     *
+     * 前序和后序基本上差不多
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> postorderTraversal1(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            TreeNode tmp = stack.pop();
+            result.add(tmp.val);
+            if (tmp.left != null) {
+                stack.push(tmp.left);
+            }
+            if (tmp.right != null) {
+                stack.push(tmp.right);
+            }
+        }
+        Collections.reverse(result);
+        return result;
     }
 }
